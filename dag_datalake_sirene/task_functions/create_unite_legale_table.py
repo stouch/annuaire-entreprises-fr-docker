b99beb8a7878fc009f1490b9f6377ac5aaa86926
Sylvain Touchard <syl20.touchard@gmail.com>
2023-03-13 17:07:47 +0100

    Remove comments

--- dag_datalake_sirene/task_functions/create_unite_legale_table.py
+++ dag_datalake_sirene/task_functions/create_unite_legale_table.py
@@ -33,7 +33,7 @@ from dag_datalake_sirene.task_functions.global_variables import DATA_DIR
 
 
 def create_unite_legale_table(**kwargs):
-    """     sqlite_client = create_table_model(
+    sqlite_client = create_table_model(
         table_name="unite_legale",
         create_table_query=create_table_unite_legale_query,
         create_index_func=create_unique_index,
@@ -49,10 +49,8 @@ def create_unite_legale_table(**kwargs):
                 f"************ {row} total records have been added "
                 f"to the unité légale table!"
             )
-
-    del df_unite_legale """
-    sqlite_client = SqliteClient(SIRENE_DATABASE_LOCATION)
-
+    del df_unite_legale
+    
     for count_unites_legales in sqlite_client.execute(get_table_count("unite_legale")):
         logging.info(
             f"************ {count_unites_legales} total records have been added to the "
